c25ee8d2b7621691c2e24315cfb70e7b5e4dc906
mersdev <127180693+mersdev@users.noreply.github.com>
2026-06-29 00:21:54 +0800

    fix: remove shared at logs

--- main.ts
+++ main.ts
@@ -37,7 +37,6 @@ namespace esp8266 {
         rxData = ""
 
         // Send the command and end with "\r\n".
-        console.log("> " + command)
         serial.writeString(command + "\r\n")
         
         // Don't check if expected response is not specified.
@@ -109,8 +108,6 @@ namespace esp8266 {
                 // Check if expected response received.
                 if (rxData.slice(0, rxData.indexOf("\r\n")).includes(response)) {
                     responseLine = rxData.slice(0, rxData.indexOf("\r\n"))
-                    console.log("< " + responseLine)
-
                     // Trim the Rx data for next call.
                     rxData = rxData.slice(rxData.indexOf("\r\n") + 2)
                     break
